bba4d752f7f7d97ddbc8c9c0934d8867cc621cbf
par1tet <mankova486@gmail.com>
2024-08-01 13:17:56 +0300

    first commit

---
+++ houtask-react/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from 'react'
+import { SideBar } from './components/SideBar.jsx'
+
+function App() {
+  return (
+    <>
+      <SideBar />
+    </>
+  )
+}
+
+export default App
---
+++ houtask-react/src/components/SideBar.jsx
@@ -0,0 +1,7 @@
+import React from "react"
+
+export const SideBar = (props) => {
+    return (<div className="sidebar">
+        any
+    </div>)
+}
