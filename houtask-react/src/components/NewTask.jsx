b2fe4fd588160cf186cecb348b90df7630e1a9b9
par1tet <mankova486@gmail.com>
2024-08-03 14:56:31 +0300

    start new task is complited

--- houtask-react/src/components/NewTask.jsx
+++ houtask-react/src/components/NewTask.jsx
@@ -8,17 +8,22 @@ export const NewTask = (props) => {
             </span>
             <div className="contentbar__new-content">
                 <div className="contentbar__new-content-section">
-                    choice goal
+                    Choice goal
                     <select>
                         <option value="Отдых">Отдых</option>
                         <option value="Еще чета">Еще чета</option>
                     </select>
                 </div>
                 <div className="contentbar__new-content-desc">
-
+                    Add description
+                    <textarea name="" id="" cols="30" rows="10" spellcheck="false"></textarea>
+                </div>
+                <div className="contentbar__new-content-tags">
+                    Add tags
+                    <textarea name="" id="" cols="30" rows="5" spellcheck="false"></textarea>
                 </div>
                 <div className="contentbar__new-content-starttask">
-
+                    <button>Start task</button>
                 </div>
             </div>
         </div>
