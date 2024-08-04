a96e29ed6f3846b4939f3044f8cb52d3a272f8b2
par1tet <mankova486@gmail.com>
2024-08-04 15:02:03 +0300

    sidebar update

--- houtask-react/src/components/SideBar.jsx
+++ houtask-react/src/components/SideBar.jsx
@@ -2,6 +2,24 @@ import React from "react"
 
 export const SideBar = (props) => {
     return (<div className="sidebar">
-        any
+        <div className="sidebar__profile">
+
+        </div>
+        <nav className="sidebar__profile_navigation">
+            <div>
+                <div className="sidebar__profile_navigation_item">
+                    Tasks
+                </div>
+                <div className="sidebar__profile_navigation_item">
+                    Goals
+                </div>
+                <div className="sidebar__profile_navigation_item">
+                    Statistic
+                </div>
+            </div>
+            <div className="sidebar__profile_navigation_item">
+                Settings
+            </div>
+        </nav>
     </div>)
 }
